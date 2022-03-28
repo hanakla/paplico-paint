d713e3d53c772b6fce5df4e0504c4a5d7bce5dac
Hanakla <8597982+hanakla@users.noreply.github.com>
2022-03-28 18:57:03 +0900

    ignore build errors

--- pkgs/silk-web/next.config.js
+++ pkgs/silk-web/next.config.js
@@ -2,25 +2,31 @@ const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
 const withTranspileModules = require('next-transpile-modules')([
-  "silk-core",
-  "silk-ui"
+  'silk-core',
+  'silk-ui',
 ])
 const { i18n } = require('./next-i18next.config')
 
-module.exports = withTranspileModules(withPWA({
+/** @type {import('next').NextConfig} */
+const config = {
   i18n,
   pwa: {
     dest: 'public',
     runtimeCaching,
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
       'silk-core': 'silk-core/src/index.ts',
-      'silk-ui': 'silk-ui/src/index.ts'
+      'silk-ui': 'silk-ui/src/index.ts',
     }
     config.plugins.push(new ForkTsCheckerWebpackPlugin())
 
     return config
-  }
-}))
+  },
+}
+
+module.exports = withTranspileModules(withPWA(config))

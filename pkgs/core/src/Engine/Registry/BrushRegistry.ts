b1c91177eaa6671c97fa34d532432db4a7531882
Hanakla <8597982+hanakla@users.noreply.github.com>
2023-10-21 20:41:00 +0900

    refactor: Move registories to Engine/Registry

--- pkgs/core/src/Engine/Registry/BrushRegistry.ts
+++ pkgs/core/src/Engine/Registry/BrushRegistry.ts
@@ -1,36 +1,38 @@
 import { Emitter } from '@/utils/Emitter'
-import { IInk, InkClass } from './Ink'
+import { catchToLog } from '@/utils/error'
+import { BrushClass, IBrush } from '../Brush'
 
 type Events = {
   entriesChanged: void
 }
 
-export class InkRegistry extends Emitter<Events> {
-  protected brushes: Map<string, InkClass> = new Map()
-  protected instances: WeakMap<InkClass, IInk> = new WeakMap()
+export class BrushRegistry extends Emitter<Events> {
+  protected brushes: Map<string, BrushClass> = new Map()
+  protected instances: WeakMap<BrushClass, IBrush> = new WeakMap()
 
-  public async register(Brush: InkClass) {
+  public async register(Brush: BrushClass) {
     try {
       this.brushes.set(Brush.id, Brush)
 
       const brush = new Brush()
-      await brush.initialize()
+      await brush.initialize({ gl: this.gl })
 
       this.instances.set(Brush, brush)
     } catch (e) {
       this.brushes.delete(Brush.id)
       this.instances.delete(Brush)
+      console.warn('Failed to register brush', Brush, e)
       throw e
     }
 
     this.emit('entriesChanged')
   }
 
-  public get brushEntries(): InkClass[] {
+  public get brushEntries(): BrushClass[] {
     return [...this.brushes.values()]
   }
 
-  public getInstance(id: string): IInk | null {
+  public getInstance(id: string): IBrush | null {
     const Class = this.brushes.get(id)
     if (!Class) return null
 
--- pkgs/core/src/Engine/Registry/InkRegistry.ts
+++ pkgs/core/src/Engine/Registry/InkRegistry.ts
@@ -1,38 +1,36 @@
 import { Emitter } from '@/utils/Emitter'
-import { catchToLog } from '@/utils/error'
-import { BrushClass, IBrush } from './Brush'
+import { IInk, InkClass } from '../Ink'
 
 type Events = {
   entriesChanged: void
 }
 
-export class BrushRegistry extends Emitter<Events> {
-  protected brushes: Map<string, BrushClass> = new Map()
-  protected instances: WeakMap<BrushClass, IBrush> = new WeakMap()
+export class InkRegistry extends Emitter<Events> {
+  protected brushes: Map<string, InkClass> = new Map()
+  protected instances: WeakMap<InkClass, IInk> = new WeakMap()
 
-  public async register(Brush: BrushClass) {
+  public async register(Brush: InkClass) {
     try {
       this.brushes.set(Brush.id, Brush)
 
       const brush = new Brush()
-      await brush.initialize({ gl: this.gl })
+      await brush.initialize()
 
       this.instances.set(Brush, brush)
     } catch (e) {
       this.brushes.delete(Brush.id)
       this.instances.delete(Brush)
-      console.warn('Failed to register brush', Brush, e)
       throw e
     }
 
     this.emit('entriesChanged')
   }
 
-  public get brushEntries(): BrushClass[] {
+  public get brushEntries(): InkClass[] {
     return [...this.brushes.values()]
   }
 
-  public getInstance(id: string): IBrush | null {
+  public getInstance(id: string): IInk | null {
     const Class = this.brushes.get(id)
     if (!Class) return null
 

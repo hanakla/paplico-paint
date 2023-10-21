5d0223244c0e15f79a68415bf282b493bf5856f1
Hanakla <8597982+hanakla@users.noreply.github.com>
2023-10-21 20:57:54 +0900

    misc: Catch errors in MixerPipeline

--- pkgs/core/src/Engine/MixerPipeline.ts
+++ pkgs/core/src/Engine/MixerPipeline.ts
@@ -1,5 +1,9 @@
 import { CompositeMode, PaplicoDocument } from '@/Document'
-import { saveAndRestoreCanvas, setCanvasSize } from '@/utils/canvas'
+import {
+  freeingCanvas,
+  saveAndRestoreCanvas,
+  setCanvasSize,
+} from '@/utils/canvas'
 import { imageSourceToBlob } from '@/utils/DebugHelper'
 import { rescue } from '@/utils/resque'
 import { BrushRegistry } from './Registry/BrushRegistry'
@@ -8,6 +12,7 @@ import { RenderCycleLogger } from './RenderCycleLogger'
 import { RenderPhase, Renderer } from './Renderer'
 import { RuntimeDocument } from './RuntimeDocument'
 import { Viewport } from './types'
+import { PaplicoAbortError } from '@/Errors'
 
 export class MixerPipeline {
   protected brushRegistry: BrushRegistry
@@ -36,7 +41,7 @@ export class MixerPipeline {
   public async fullyRender(
     dest: CanvasRenderingContext2D,
     doc: RuntimeDocument,
-    render: Renderer,
+    renderer: Renderer,
     {
       abort,
       viewport,
@@ -50,103 +55,100 @@ export class MixerPipeline {
       phase: RenderPhase
       logger: RenderCycleLogger
     },
-  ) {
-    console.log('Render starting')
+  ): Promise<void> {
     const tmp = createCanvas()
     const tmpctx = tmp.getContext('2d')!
     setCanvasSize(tmp, viewport)
 
     logger.group('MixerPipeline.fullyRender')
+
     logger.log('Destination canvas', dest.canvas)
     dest.clearRect(0, 0, dest.canvas.width, dest.canvas.height)
     logger.log('Clear dest canvas')
 
     logger.time('Render all layers time')
+    try {
+      for (const node of doc.rootNodes) {
+        const layer = doc.resolveLayer(node.layerUid)
 
-    for (const node of doc.rootNodes) {
-      const layer = doc.resolveLayer(node.layerUid)
-
-      if (!layer) {
-        logger.error('Bad layer link', node.layerUid)
-        continue
-      }
-
-      logger.log(
-        'Render layer',
-        layer.source.uid,
-        layer.source.name,
-        layer.source.layerType,
-      )
-
-      logger.time(`Render layer time: ${layer.source.uid}`)
-
-      let image: ImageBitmap | HTMLCanvasElement | null | void
-
-      if (override?.[layer.source.uid]) {
-        image = override[layer.source.uid]
-      } else if (layer.source.layerType === 'raster') {
-        image = (await doc.getOrCreateLayerBitmapCache(layer.source.uid))!
-      } else if (layer.source.layerType === 'vector') {
-        const requestSize = { width: viewport.width, height: viewport.height }
-
-        if (doc.hasLayerBitmapCache(layer.source.uid, requestSize)) {
-          logger.info('Use cached bitmap for vector layer', layer.source.uid)
-          image = (await doc.getOrCreateLayerBitmapCache(
-            layer.source.uid,
-            requestSize,
-          ))!
-        } else {
-          logger.info(
-            'Cache is invalidated, re-render vector layer',
-            layer.source.uid,
-          )
-
-          await render.renderVectorLayer(tmp, layer.source, {
-            viewport,
-            abort,
-            phase,
-            logger,
-          })
-
-          image = await doc.updateOrCreateLayerBitmapCache(
-            layer.source.uid,
-            tmpctx.getImageData(0, 0, viewport.width, viewport.height),
-          )
+        if (!layer) {
+          logger.error('Bad layer link', node.layerUid)
+          continue
         }
-      } else {
-        // TODO
-        continue
-      }
 
-      logger.timeEnd(`Render layer time: ${layer.source.uid}`)
-
-      const mode = layerCompositeModeToCanvasCompositeMode(
-        layer.source.compositeMode,
-      )
-
-      await saveAndRestoreCanvas(dest, async () => {
         logger.log(
-          `Will draw layer ${layer.source.uid} as ${mode} to destination.`,
-          image,
+          'Render layer',
+          layer.source.uid,
+          layer.source.name,
+          layer.source.layerType,
         )
-        dest.globalCompositeOperation = mode
-        const result = rescue(() => dest.drawImage(image!, 0, 0))
-        rescue.isFailure(result) &&
-          logger.error('drawImage error', result.error)
 
-        // const img = await imageSourceToBlob(image!)
-        // logger.log(
-        //   `image: %o %c+`,
-        //   img.imageUrl,
-        //   `font-size: 0px; padding: 64px; color: transparent; background: url(${img.imageUrl}) center/contain no-repeat; border: 1px solid #444;`,
-        //   ''
-        // )
-      })
+        logger.time(`Render layer time: ${layer.source.uid}`)
+
+        let image: ImageBitmap | HTMLCanvasElement | null | void
+
+        if (override?.[layer.source.uid]) {
+          image = override[layer.source.uid]
+        } else if (layer.source.layerType === 'raster') {
+          image = (await doc.getOrCreateLayerBitmapCache(layer.source.uid))!
+        } else if (layer.source.layerType === 'vector') {
+          const requestSize = { width: viewport.width, height: viewport.height }
+
+          if (doc.hasLayerBitmapCache(layer.source.uid, requestSize)) {
+            logger.info('Use cached bitmap for vector layer', layer.source.uid)
+            image = (await doc.getOrCreateLayerBitmapCache(
+              layer.source.uid,
+              requestSize,
+            ))!
+          } else {
+            logger.info(
+              'Cache is invalidated, re-render vector layer',
+              layer.source.uid,
+            )
+
+            await renderer.renderVectorLayer(tmp, layer.source, {
+              viewport,
+              abort,
+              phase,
+              logger,
+            })
+
+            if (abort?.aborted) throw new PaplicoAbortError()
+
+            image = await doc.updateOrCreateLayerBitmapCache(
+              layer.source.uid,
+              tmpctx.getImageData(0, 0, viewport.width, viewport.height),
+            )
+          }
+        } else {
+          // TODO
+          continue
+        }
+
+        logger.timeEnd(`Render layer time: ${layer.source.uid}`)
+
+        const mode = layerCompositeModeToCanvasCompositeMode(
+          layer.source.compositeMode,
+        )
+
+        await saveAndRestoreCanvas(dest, async () => {
+          logger.log(
+            `Will draw layer ${layer.source.uid} as ${mode} to destination.`,
+            image,
+          )
+          dest.globalCompositeOperation = mode
+
+          const result = rescue(() => dest.drawImage(image!, 0, 0))
+          rescue.isFailure(result) &&
+            logger.error('drawImage error', result.error)
+        })
+      }
+    } finally {
+      freeingCanvas(tmp)
+
+      logger.timeEnd('Render all layers time')
+      logger.groupEnd()
     }
-
-    logger.timeEnd('Render all layers time')
-
-    logger.groupEnd()
   }
 
   // public async render() {}
